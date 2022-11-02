ae920eb0966a0e962bc1d0af2f6318ae0db46791
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-02 12:47:26 -0600

    homepage animations

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,23 +1,68 @@
-import './index.scss'
-import 'animate.css';
-import { Link } from 'react-router-dom';
-import logo from "../../assests/images/logo-s.png"
+import "./index.scss";
+import "animate.css";
+import { Link } from "react-router-dom";
+import logo from "../../assests/images/logo-s.png";
+import { useState, useEffect } from "react";
+import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ['a', 'l', 'm', 'a'];
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ];
+
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+  
+
   return (
     <div className="container home-page">
       <div className="text-zone">
-        <h1>Hi, <br/> I'm
-        <img src={logo} alt="developer"/>
-        alma
-        <br />
-        Web Developer
+        <h1>
+          <span className={letterClass}>H</span> 
+          <span className={`${letterClass} _12`}>i,</span> 
+          <br />
+          <span className={`${letterClass} _13`}>I</span> 
+          <span className={`${letterClass} _14`}>m</span> 
+
+          <img src={logo} alt="developer" />
+          <AnimatedLetters 
+          letterClass={letterClass}
+          strArray={nameArray}
+          idx={15}
+           />
+          <br />
+          <AnimatedLetters 
+          letterClass={letterClass}
+          strArray={jobArray}
+          idx={22}
+           />
         </h1>
         <h2>tag line here....... </h2>
-        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   );
-}
+};
 
-export default Home
+export default Home;
