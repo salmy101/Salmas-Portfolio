1442a752c9fc4fa3e10c9e236d933f9c2f632fc8
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2022-11-24 13:22:19 -0700

    about page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 // import Sidebar from './components/Sidebar';
 import Home from "./components/Home";
+import About from "./components/About";
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+
         </Route>
       </Routes>
     </>
---
+++ src/components/About/index.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import AnimatedLetters from "../AnimatedLetters";
+import "./index.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngular, faCss3, faGitAlt, faGithub, faHtml5, faJsSquare, faReact } from "@fortawesome/free-brands-svg-icons";
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+  return (
+    <div className="container about-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={["A", "b", "o", "u", "t", " ", "m", "e"]}
+            idx={15}
+          />
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
+          egestas sed sed risus pretium. Nulla facilisi etiam
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
+          egestas sed sed risus pretium. Nulla facilisi etiam
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus
+          egestas sed sed risus pretium. Nulla facilisi etiam
+        </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
